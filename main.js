5f2be52fc7725096c21baefd5e9b874ddea3ecdc
thedon122 <donovan122@yahoo.com>
2018-03-28 15:37:52 -0400

    using javaScript change the colors after every turn

--- main.js
+++ main.js
@@ -1,24 +1,64 @@
 $(document).ready(function(){
     let cssObject
-    // function to turn cicrles red if they do not have a color
+    
     const redClick = function (cssObject){
-        $(cssObject).css("background-color", "red")
+        console.log('This is cssObject', cssObject) 
+        $(`#${cssObject}`).css("background-color", "red")
     }
     // function to turn cicrles black if they do not have a color
      const blackClick = function (cssObject){
-        // if( !($(cssObject).css("background-color", "black")) || !($(cssObject).css("background-color", "red"))){
+        
         console.log('This is cssObject', cssObject)    
         $(`#${cssObject}`).css("background-color", "black")
         
-        // }
+        
         
     }
+    let gameBroadArray = []
+    let alphabet
+    alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
+    for(i = 0; i<7; i++){
+        let alp
+        alp = 0
+        gameBroadArray[i] = alphabet[alp] + (i+1)
+        for(j = 0; j<6; j++){
+            alp++
+            gameBroadArray[i][j] = alphabet[alp] + (j+1)
+        }
+        
+    }
+    console.log(gameBroadArray)
     // function calls on function to cicrles to change when click
+    // const count = function(turnCount){  
+    let turn = 0
     $('.cicrle').on('click', function() {
         console.log(event.target.id)
+        console.log(turn)
         let circleToChange = event.target.id
+        if(turn%2 == 0){
         blackClick(circleToChange)
+        turn++
+        }
+        else
+        {redClick(circleToChange)
+        turn++}
+        // var turn = 42 - turnCount
+        // if(turnCount%2 === 0){
+
+        
     })
+    
+        //     console.log(turn)
+        //     $('.cicrle').on('click', function() {
+        //         console.log(event.target.id)
+        //         turn++
+        //         console.log(turn)
+        //         let circleToChange = event.target.id
+        //         redClick(circleToChange)
+        
+        // })
+    // }
+    // turn + '%'
+
     // cssObject =$('#a1')
-    $(cssObject).click(blackClick(cssObject))
 })
