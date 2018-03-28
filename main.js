423038e1aaecc81ac8cc3ffcb3794a08eda7b507
thedon122 <donovan122@yahoo.com>
2018-03-28 12:56:42 -0400

    using javaScript change the color of the circles

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-
+    let cssObject
+    // function to turn cicrles red if they do not have a color
+    const redClick = function (cssObject){
+        $(cssObject).css("background-color", "red")
+    }
+    // function to turn cicrles black if they do not have a color
+     const blackClick = function (cssObject){
+        // if( !($(cssObject).css("background-color", "black")) || !($(cssObject).css("background-color", "red"))){
+        console.log('This is cssObject', cssObject)    
+        $(`#${cssObject}`).css("background-color", "black")
+        
+        // }
+        
+    }
+    // function calls on function to cicrles to change when click
+    $('.cicrle').on('click', function() {
+        console.log(event.target.id)
+        let circleToChange = event.target.id
+        blackClick(circleToChange)
+    })
+    // cssObject =$('#a1')
+    $(cssObject).click(blackClick(cssObject))
 })
